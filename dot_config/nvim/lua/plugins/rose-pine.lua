4b865d67f01695b3c822118003020e3ca17ec8c3
Taras Dyshkant <hitori.gm@gmail.com>
2024-09-14 15:48:07 +0300

    nvim: switch to astro

---
+++ dot_config/nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,11 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  {
+    "rose-pine",
+    opts = {
+      styles = {
+        italic = false,
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        sorting_strategy = "descending",
+        layout_config = {
+          horizontal = { prompt_position = "bottom" },
+        },
+      },
+    },
+  },
+}
