6507b1ef778993bf50a8bfdc7dccbe9ed4ee6ad6
Taras Dyshkant <hitori.gm@gmail.com>
2024-12-16 12:59:35 +0200

    nvim: migrate to astro

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,8 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   {
-    "rose-pine",
-    opts = {
-      styles = {
-        italic = false,
-      },
-    },
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
   },
 }
