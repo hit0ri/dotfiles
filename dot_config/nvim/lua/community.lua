a268d41c110dd18c8b71552f55f9ab9c90c09c8a
Taras Dyshkant <hitori.gm@gmail.com>
2024-12-19 11:32:23 +0200

    nvim: add render-markdown plugin

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -18,6 +18,7 @@ return {
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.json" },
