2229400ed0d0c30a3ea7a62c722733d66ccfdc8a
Taras Dyshkant <hitori.gm@gmail.com>
2025-01-27 11:38:05 +0200

    nvim: drop ruby extras

--- dot_config/nvim-lazy/lua/plugins/extras.lua
+++ dot_config/nvim-lazy/lua/plugins/extras.lua
@@ -13,7 +13,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.ruby" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.zig" },
