d929dabf4dd096942004b83c7d8e7e9c5678d86c
Taras Dyshkant <hitori.gm@gmail.com>
2025-02-19 14:39:48 +0200

    nvim-lazy: add groovy support

---
+++ dot_config/nvim-lazy/lua/plugins/groovy.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "groovy" } },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "npm-groovy-lint", "groovy-language-server" } },
+  },
+
+  -- Disabled due to performance issues
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   optional = true,
+  --   opts = {
+  --     linters_by_ft = {
+  --       Jenkinsfile = { "npm-groovy-lint" },
+  --       groovy = { "npm-groovy-lint" },
+  --     },
+  --   },
+  -- },
+  --
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     default_format_opts = {
+  --       timeout_ms = 5000,
+  --     },
+  --     formatters_by_ft = {
+  --       Jenkinsfile = { "npm-groovy-lint" },
+  --       groovy = { "npm-groovy-lint" },
+  --     },
+  --   },
+  -- },
+}
