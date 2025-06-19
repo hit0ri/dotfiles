84a66999be77a7713ccfb6b20ce116bf42cdf7d9
Taras Dyshkant <hitori.gm@gmail.com>
2025-06-19 20:25:42 +0300

    zsh: configure skim

---
+++ dot_config/sk/themes/catppuccin-latte.sh
@@ -0,0 +1,2 @@
+export SKIM_DEFAULT_OPTIONS="$SKIM_DEFAULT_OPTIONS \
+--color=fg:#4c4f69,bg:#eff1f5,matched:#ccd0da,matched_bg:#dd7878,current:#4c4f69,current_bg:#bcc0cc,current_match:#eff1f5,current_match_bg:#dc8a78,spinner:#40a02b,info:#8839ef,prompt:#1e66f5,cursor:#d20f39,selected:#e64553,header:#179299,border:#9ca0b0"
