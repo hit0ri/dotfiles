da9f321ad2c9cc462a854bacfe79380cf483b5ee
Taras Dyshkant <taras.dyshkant@tui.co.uk>
2025-06-23 10:58:47 +0300

    Revert "zsh: configure skim"
    This reverts commit 4df3ba57b05080f8b93a846aa4990e5516515951.

--- dot_config/sk/themes/catppuccin-macchiato.sh
+++
@@ -1,2 +0,0 @@
-export SKIM_DEFAULT_OPTIONS="$SKIM_DEFAULT_OPTIONS \
---color=fg:#cad3f5,bg:#24273a,matched:#363a4f,matched_bg:#f0c6c6,current:#cad3f5,current_bg:#494d64,current_match:#24273a,current_match_bg:#f4dbd6,spinner:#a6da95,info:#c6a0f6,prompt:#8aadf4,cursor:#ed8796,selected:#ee99a0,header:#8bd5ca,border:#6e738d"
