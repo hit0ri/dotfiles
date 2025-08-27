dd5876e5847eca0ac904c415592be778e9a6f589
t.dyshkant <hitori.gm@gmail.com>
2025-08-28 00:50:33 +0300

    nvim: fix hclfmt with new terragrunt

--- dot_config/nvim-lazy/lua/plugins/conform.lua
+++ dot_config/nvim-lazy/lua/plugins/conform.lua
@@ -5,6 +5,9 @@ return {
       shfmt = {
         prepend_args = { "-i", "4", "-ci" },
       },
+      terragrunt_hclfmt = {
+        args = { "hcl", "fmt", "--file", "$FILENAME" },
+      },
     },
   },
 }
